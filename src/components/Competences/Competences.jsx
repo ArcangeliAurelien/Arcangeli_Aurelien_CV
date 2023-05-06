881e06faf3ffa4917e9a2e4ef33fe74804875895
Aurélien Arcangeli <arkaurel5929@gmail.com>
2023-05-06 18:53:49 +0200

    modif

--- src/components/Competences/Competences.jsx
+++ src/components/Competences/Competences.jsx
@@ -1,54 +1,51 @@
 import React from 'react'
 import './Competences.css'
-import Fade from 'react-reveal/Fade'
 
 export default function Competences() {
 
   return (
     <div className='competences' id='competences'>
       <div className='titre'>Compétences</div>
-      <div>
-        <Fade bottom>
-          <div className="containerComp">
-            <div className='sousTitre'>Front-end</div>
-            <p>
-              Création et intégration de sites web et applications mobiles responsive, accessibles et répondant aux normes W3C.
-            </p>
-            <div className='list'>
-              <ul>
-                <li>HTML5</li>
-                <li>CSS3</li>
-                <li>Javascript</li>
-                <li>JSON</li>
-              </ul>
-              <ul>
-                <li>Sass</li>
-                <li>Bootstrap</li>
-                <li>React</li>
-                <li>Figma</li>
-              </ul>
-            </div>
+      <div className='containerComp'>
+        <div className="cont">
+          <div className='sousTitre'>Front-end</div>
+          <p>
+            Création et intégration de sites web et applications mobiles responsive, accessibles et répondant aux normes W3C.
+          </p>
+          <div className='list'>
+            <ul>
+              <li>HTML5</li>
+              <li>CSS3</li>
+              <li>Javascript</li>
+              <li>JSON</li>
+            </ul>
+            <ul>
+              <li>Sass</li>
+              <li>Bootstrap</li>
+              <li>React</li>
+              <li>Figma</li>
+            </ul>
           </div>
+        </div>
 
-          <div className="containerComp">
-            <div className='sousTitre'>Savoir-Être</div>
-            <p>
-              Ensemble de qualités professionnelles qui reflètent la manière dont vous vous comportez dans un environnement professionnel.
-            </p>
-            <div className='list'>
-              <ul>
-                <li>Capacité d'adaptation</li>
-                <li>Travail d'équipe</li>
-                <li>Autonomie</li>
-              </ul>
-              <ul>
-                <li>Curiosité</li>
-                <li>Attention du détail</li>
-                <li>Disponibilité</li>
-              </ul>
-            </div>
+        <div className="cont">
+          <div className='sousTitre'>Savoir-Être</div>
+          <p>
+            Ensemble de qualités professionnelles qui reflètent la manière dont vous vous comportez dans un environnement professionnel.
+          </p>
+          <div className='list'>
+            <ul>
+              <li>Capacité d'adaptation</li>
+              <li>Travail d'équipe</li>
+              <li>Autonomie</li>
+            </ul>
+            <ul>
+              <li>Curiosité</li>
+              <li>Attention du détail</li>
+              <li>Disponibilité</li>
+            </ul>
           </div>
-        </Fade>
+        </div>
       </div>
     </div>
   )
--- src/components/Parcours/Parcours.jsx
+++ src/components/Parcours/Parcours.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import './Parcours.css'
+import Formations from '../../data/Parcours.json'
 
 export default function Parcours() {
+  const element = Formations.formations.map((ele) => ele.titre)
+  console.log(element);
+
   return (
     <div className='parcours' id='parcours'>
       <div className='titre'>Parcours</div>
+      <div className='containerParcours'>
+        <div className=' box box1'>
+          <h2>Formations</h2>
+          {
+            <div>
+              {element}
+            </div>
+          }
+        </div>
 
+        <div className='box box2'>
+          <h2>Expériences Professionelle</h2>
+        </div>
+      </div>
       
     </div>
   )
