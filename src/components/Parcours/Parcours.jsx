b5f5f25bd8c5d15e054df5fcc139de84753c4902
Aurélien Arcangeli <arkaurel5929@gmail.com>
2023-05-17 16:18:24 +0200

    Parcours

--- src/components/Parcours/Parcours.jsx
+++ src/components/Parcours/Parcours.jsx
@@ -1,26 +1,52 @@
 import React from 'react'
 import './Parcours.css'
-import Formations from '../../data/Parcours.json'
+import { formations, experiencePro } from '../../data/Parcours.js'
 
 export default function Parcours() {
-  const element = Formations.formations.map((ele) => ele.titre)
-  console.log(element);
+  formations.map((ele) => console.log(ele.titre))
 
   return (
     <div className='parcours' id='parcours'>
       <div className='titre'>Parcours</div>
       <div className='containerParcours'>
         <div className=' box box1'>
-          <h2>Formations</h2>
-          {
-            <div>
-              {element}
-            </div>
-          }
+          <h2>Formations</h2><br />
+          <div>
+            {formations.map((data, key) => {
+              return (
+                <div key={key}>
+                  <ul>
+                    <li>
+                      <h3>{data.date}</h3>
+                      <div>{data.lieu}: <b>{data.titre}</b></div>
+                    </li><br />
+                  </ul>
+                </div>
+              )
+            })}
+          </div>
         </div>
 
         <div className='box box2'>
           <h2>Expériences Professionelle</h2>
+          <div>
+            {experiencePro.map((data, key) => {
+              return (
+                <div key={key}>
+                  <ul>
+                    <li>
+                      <h3>{data.titre}</h3>
+                      <div>{data.date}</div>
+                      <div>{data.lieu}</div>
+                      {(data.descriptif).map((descEle, index) => (<ul>
+                        <li key={index}>{descEle}</li></ul>
+                      ))}
+                    </li><br />
+                  </ul>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
       
