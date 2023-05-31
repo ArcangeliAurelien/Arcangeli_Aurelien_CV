07f41dbe2bf479c931999e460af472b7e5171d43
Aurélien Arcangeli <arkaurel5929@gmail.com>
2023-05-31 15:55:56 +0200

    Navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,62 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Navbar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouseChimney } from '@fortawesome/free-solid-svg-icons'
+import { faHouseChimney, faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu)
+  }
+
+  useEffect(() => {
+
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+
+      if (window.innerWidth > 900) {
+        setToggleMenu(false)
+      }
+    }
+
+    window.addEventListener('resize', changeWidth)
+
+    return () => {
+      window.removeEventListener('resize', changeWidth)
+    }
+
+  }, [])
+
   return (
-    <nav className='nav'>
-      <ul>
-        <li>
-          <a href='#home' className='active'>
-            <FontAwesomeIcon icon={faHouseChimney} />
-          </a>
-        </li>
-        <li>
-          <a href='#aPropos'>À propos</a>
-        </li>
-        <li>
-          <a href="#competences">Compétences</a>
-        </li>
-        <li>
-          <a href="#parcours">Parcours</a>
-        </li>
-        <li>
-          <a href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+    <nav>
+      {(toggleMenu || screenWidth > 900) && (
+        <ul className="listNav">
+          <li className="items">
+            <a href='#home'>
+             <FontAwesomeIcon icon={faHouseChimney} />
+            </a>
+          </li>
+          <li className="items">
+            <a href='#aPropos'>À propos</a>
+          </li>
+          <li className="items">
+            <a href="#competences">Compétences</a>
+          </li>
+          <li className="items">
+            <a href="#parcours">Parcours</a>
+          </li>
+          <li className="items">
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li className="items">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      )}
+      <FontAwesomeIcon icon={faBars} className='menu_hamburger' onClick={toggleNav} />
     </nav>
   )
 }
